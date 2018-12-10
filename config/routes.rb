ab3f01b21a5bc95f3835104767838a14857ef0aa
Nicolas Debray <nicdebray@gmail.com>
2018-12-10 11:14:30 +0100

    added homepage and adapted the routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get 'about', to: 'pages#about', as: :about
   get 'contact', to: 'pages#contact', as: :contact
-  get 'todos/index'
+  get 'todos', to: 'todos#index'
   get 'todos/show'
   get 'todos/new'
   get 'todos/create'
@@ -9,4 +9,5 @@ Rails.application.routes.draw do
   get 'todos/update'
   get 'todos/destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'pages#home'
 end
