f978df0d3ff9b4314ff0c9726e2247967f047ff8
Nicolas Debray <nicdebray@gmail.com>
2018-12-10 10:36:29 +0100

    odel and controller created

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  get 'todos/index'
+  get 'todos/show'
+  get 'todos/new'
+  get 'todos/create'
+  get 'todos/edit'
+  get 'todos/update'
+  get 'todos/destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
