182c48029a3729171eaf278795da0ddc110b3310
Nicolas Debray <nicdebray@gmail.com>
2018-12-16 21:34:14 +0100

    adapted the weather return and info

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -5,13 +5,22 @@ class Todo < ApplicationRecord
   validates :title, presence: true
   validates :description, presence: true
 
-def weather
+def weather_temp
   weather_key = ENV['openweathermap_api_key']
   url = "https://api.openweathermap.org/data/2.5/weather?q=brussels&APPID=#{weather_key}"
   user_serialized = open(url).read
   user = JSON.parse(user_serialized)
 
   records = user["main"]["temp"] - 273.15
+  temp = records.round(1)
 end
 
+def weather_location
+  weather_key = ENV['openweathermap_api_key']
+  url = "https://api.openweathermap.org/data/2.5/weather?q=brussels&APPID=#{weather_key}"
+  user_serialized = open(url).read
+  user = JSON.parse(user_serialized)
+
+  records = user["name"]
+end
 end
