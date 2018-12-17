69a1bd710f76a82f0631b72f78bf693e947d6d2d
Nicolas Debray <nicdebray@gmail.com>
2018-12-17 17:59:49 +0100

    added the info to a dropdown menu for the avatar icon and added some shadow to the cards

--- app/javascript/packs/after_load.js
+++ app/javascript/packs/after_load.js
@@ -25,5 +25,19 @@ function createNewTodoClick(){
 }
 
 createNewTodoClick();
+// End toggle create new todo
 
 
+// toggle avatar menu
+function avatarMenuOpen() {
+  const avatarmenu = document.querySelector(".user-menu");
+  avatarmenu.classList.toggle("hidden");
+}
+
+function showAvatarMenuClick() {
+  const avataricon = document.querySelector(".navbar-todo-avatar");
+  avataricon.addEventListener("click", avatarMenuOpen);
+}
+
+showAvatarMenuClick();
+// End toggle avatar menu
